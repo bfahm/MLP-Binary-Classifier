effda83eb09164cd9f60f7acbc1542a996598767
bola <bola_fah.97@hotmail.com>
2019-06-22 18:51:46 +0200

    code cleaning

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 from data import *
-from pandas.plotting import scatter_matrix
-import matplotlib.pyplot as plt
-from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 import random
-
-# seed = 7  # Retrieve the same results every run
-# scoring = 'accuracy'  # Test model score by accuracy (in %percentage)
 
 
 def logistic_model(X_train, X_test, Y_train):
@@ -28,13 +19,10 @@ def neural_model(X_train, X_test, Y_train):
     predictions = classifier.predict(X_test)
     return classifier, predictions
 
-def print_model_specs(predictions, Y_test):
-    print("Accuracy is: ", accuracy_score(Y_test, predictions))
-    #print("------------------------------------------------------------------")
-    #print(classification_report(Y_test, predictions))
-
 
 def drop_features(train_x, test_x, needed):
+    # Needed refers to a the number of random features to be generated. In practice, the generated features
+    # are much less due to duplicates in random choosing, but it helped in choosing the features roughly.
     all_columns = ['variable2_x', 'variable2_y', 'variable3_x', 'variable3_y',
        'variable8_x', 'variable8_y', 'variable11', 'variable14', 'variable15',
        'variable17', 'variable19', 'variable1_a', 'variable1_b', 'variable4_u',
@@ -65,6 +53,10 @@ def drop_features(train_x, test_x, needed):
 
 
 def choose_features(train_x, test_x, features):
+    """
+    This function is used after testing with multiple set of features in the drop_features method,
+    and then choosing the set of features with the highest accuracy.
+    """
     ret_train_x = pandas.DataFrame()
     ret_test_x = pandas.DataFrame()
 
@@ -76,19 +68,24 @@ def choose_features(train_x, test_x, features):
 
 
 def print_raw():
-    #pandas.set_option('display.expand_frame_repr', False)
-    print(train_x.columns)
-    #print(train_y)
-    print(test_x.columns)
-    #print(test_y)
+    pandas.set_option('display.expand_frame_repr', False)
+    print(train_x)
+    print(train_y)
+    print(test_x)
+    print(test_y)
+
+
+def print_model_specs(predictions, Y_test):
+    print("Accuracy is: ", accuracy_score(Y_test, predictions))
+    print("------------------------------------------------------------------")
+    print(classification_report(Y_test, predictions))
 
 
 train_x, train_y, test_x, test_y = load_dataset()
-#train_x, test_x, selected_features = drop_features(train_x, test_x, 100)
+# train_x, test_x, selected_features = drop_features(train_x, test_x, 100)
 chosen = ['variable6_d', 'variable14', 'variable7_bb', 'variable4_u', 'variable6_ff', 'variable10_f', 'variable13_g', 'variable11', 'variable6_i', 'variable6_m', 'variable2_x', 'variable9_t', 'variable7_z', 'variable9_f', 'variable7_ff', 'variable6_x', 'variable3_x', 'variable6_c', 'variable1_a', 'variable7_n', 'variable5_g', 'variable12_f', 'variable3_y', 'variable4_y', 'variable7_dd', 'variable1_b', 'variable7_h', 'variable6_cc', 'variable6_j', 'variable7_j']
 train_x, test_x = choose_features(train_x, test_x, chosen)
 classifier, predictions = neural_model(train_x, test_x, train_y)
 print_model_specs(predictions, test_y)
-#print(selected_features)
-
-#print_raw()
+# print(selected_features)
+# print_raw()
